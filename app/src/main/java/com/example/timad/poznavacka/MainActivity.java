e93e20321f1f7346114a910b5080b7ade8325d25
Timotej Adamec <tim.adamec@gmail.com>
2019-11-12 19:26:26 +0100

    nav

--- app/src/main/java/com/example/timad/poznavacka/MainActivity.java
+++ app/src/main/java/com/example/timad/poznavacka/MainActivity.java
@@ -48,7 +48,7 @@ public class MainActivity extends AppCompatActivity implements
                 break;
             }
 
-            case R.id.nav_current: {
+            case R.id.nav_practice: {
 
                 break;
             }
