a6723800a2fde468c37faefa56020a83a54107fd
Timotej Adamec <tim.adamec@gmail.com>
2019-11-20 22:14:07 +0100

    fragments in lists activity (examples) kubusovy veci z ListsActivity presunuty do MyListsFragment

--- app/src/main/java/com/example/timad/poznavacka/MyListsFragment.java
+++ app/src/main/java/com/example/timad/poznavacka/MyListsFragment.java
@@ -4,8 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -14,20 +12,10 @@ import androidx.fragment.app.Fragment;
 public class MyListsFragment extends Fragment {
     private static final String TAG = "ListsFragment";
 
-    private Button btnTEST;
-
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_mylists, container, false);
-        btnTEST = (Button) view.findViewById(R.id.btnTEST);
-
-        btnTEST.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getActivity(), "TESTING BUTTON CLICK 1", Toast.LENGTH_SHORT).show();
-            }
-        });
 
         return view;
     }
