460099bfc755bb6cb3f36a3b8138c61307be3db8
Timotej Adamec <tim.adamec@gmail.com>
2019-11-24 23:14:26 +0100

    CreateList

--- app/src/main/java/com/example/timad/poznavacka/CreateListFragment.java
+++ app/src/main/java/com/example/timad/poznavacka/CreateListFragment.java
@@ -1,59 +1,74 @@
 package com.example.timad.poznavacka;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 
 public class CreateListFragment extends Fragment {
-    private static final String TAG = "CreateFragment";
+    private static final String TAG = "CreateListFragment";
+    private static final String KEY_TITLE = "title";
+    private static final String KEY_RAD = "rad";
+    private static final String KEY_DRUH = "druh";
+    private static final String KEY_ZASTUPCE = "zastupce";
 
-    private Button btnTEST;
+    private Button btnCREATE;
+    private EditText userInputTitle;
+    private EditText userInputRepresentatives;
 
-    FirestoreImpl firestoreImpl;
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private FirestoreImpl firestoreImpl;
+
+    private ArrayList<String> representatives;
+    private String title;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_createlist, container, false);
-        btnTEST = (Button) view.findViewById(R.id.btnTEST);
+        firestoreImpl = new FirestoreImpl();
+        btnCREATE = view.findViewById(R.id.createButton);
+        userInputRepresentatives = view.findViewById(R.id.userInputRepresentatives);
+        userInputTitle = view.findViewById(R.id.userInputTitle);
 
-        btnTEST.setOnClickListener(new View.OnClickListener() {
+        btnCREATE.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String title = userInputTitle.getText().toString();
+                String rawRepresentatives = userInputRepresentatives.getText().toString();
+
+                representatives = new ArrayList<>(Arrays.asList(rawRepresentatives.split("\\s*,\\s*")));
+                Log.d(TAG, representatives.toString());
+
+
                 Toast.makeText(getActivity(), "TESTING BUTTON CLICK 1", Toast.LENGTH_SHORT).show();
             }
         });
 
 
-        // Create a new zastupce
-        Map<String, String> zkouska = new HashMap<>();
-        zkouska.put("zastupce", "zkouskaZástupce");
-        zkouska.put("druh", "zkouskaDruh");
-        zkouska.put("rad", "zkouskaŘád");
+        //test
 
         // Create a new zastupce with a reference to an image
-        Map<String, String> zkouska1 = new HashMap<>();
-        zkouska1.put("zastupce", "zkouskaZástupce");
-        zkouska1.put("druh", "zkouskaDruh");
-        zkouska1.put("rad", "zkouskaŘád");
+        /*Map<String, Object> zkouska1 = new HashMap<>();
+        zkouska1.put(KEY_TITLE, "zkouskaTitle");
+        zkouska1.put("zastupce", "zkouskaZástupce1");
+        zkouska1.put("druh", "zkouskaDruh1");
+        zkouska1.put("rad", "zkouskaŘád1");
+        zkouska1.put("imgRef", 265);*/
 
-        firestoreImpl.uploadFile(zkouska, db);
-        firestoreImpl.uploadFile(zkouska1, db);
 
-
+        //firestoreImpl.uploadData(zkouska1, "poznavackaExample");
+        //firestoreImpl.readData("poznavackaExample");
         return view;
     }
 }
--- app/src/main/java/com/example/timad/poznavacka/FirestoreImpl.java
+++ app/src/main/java/com/example/timad/poznavacka/FirestoreImpl.java
@@ -2,12 +2,17 @@ package com.example.timad.poznavacka;
 
 import android.util.Log;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.Map;
+import java.util.Objects;
 
 import androidx.annotation.NonNull;
 
@@ -15,9 +20,15 @@ import androidx.annotation.NonNull;
 public class FirestoreImpl {
     public static final String TAG = "FirestoreImpl";
 
+    private FirebaseFirestore db;
 
-    public void uploadFile(Map<String, String> zastupce, FirebaseFirestore db) {
-        db.collection("poznavackaExample")
+    public FirestoreImpl() {
+        db = FirebaseFirestore.getInstance();
+    }
+
+
+    public void uploadData(Map<String, Object> zastupce, String collectionRef) {
+        db.collection(collectionRef)
                 .add(zastupce)
                 .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
                     @Override
@@ -29,6 +40,23 @@ public class FirestoreImpl {
                     @Override
                     public void onFailure(@NonNull Exception e) {
                         Log.w(TAG, "Error adding document", e);
+                    }
+                });
+    }
+
+    public void readData(String collectionPath) {
+        db.collection(collectionPath)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : Objects.requireNonNull(task.getResult())) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
                     }
                 });
     }
