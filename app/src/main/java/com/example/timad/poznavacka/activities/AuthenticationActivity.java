0a8c3fd1b379d9015c283484df7d52d0b21f7c09
Timotej Adamec <tim.adamec@gmail.com>
2020-02-16 22:39:41 +0100

    authentication

--- app/src/main/java/com/example/timad/poznavacka/activities/AuthenticationActivity.java
+++ app/src/main/java/com/example/timad/poznavacka/activities/AuthenticationActivity.java
@@ -1,83 +1,153 @@
 package com.example.timad.poznavacka.activities;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
+import com.example.timad.poznavacka.R;
+import com.example.timad.poznavacka.activities.lists.ListsActivity;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.viewpager.widget.ViewPager;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+public class AuthenticationActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = "AuthenticationActivity";
 
-public class AuthenticationActivity extends AppCompatActivity {
-    private FirebaseAuth mAuth;
-    /*
+    private ViewPager mViewPager;
+    private static final int MY_PERMISSIONS_REQUEST_READ_STORAGE = 1;
+
+    GoogleSignInClient mGoogleSignInClient;
+    private static final int RC_SIGN_IN = 9001;
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        updateUser(currentUser);
+        setContentView(R.layout.activity_authentication);
+
+        // asks for perrmission to read and write to external storage
+        writToExternalStorage();
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestProfile()
+                .requestId()
+                .requestEmail()
+                .build();
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        SignInButton signInButton = findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_WIDE);
+        signInButton.setOnClickListener(this);
+
     }
 
+    public void writToExternalStorage() {
+        if (ContextCompat.checkSelfPermission(AuthenticationActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    AuthenticationActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(AuthenticationActivity.this);
+                builder.setTitle("request Perrmission");
+                builder.setMessage("you should enable this perrmission so we can write in to your external storage");
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ActivityCompat.requestPermissions(AuthenticationActivity.this,
+                                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_READ_STORAGE);
+                    }
+                });
+                builder.setPositiveButton("grant permission", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ActivityCompat.requestPermissions(AuthenticationActivity.this,
+                                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_READ_STORAGE);
+                    }
+                });
+                builder.show();
+            } else {
+                ActivityCompat.requestPermissions(AuthenticationActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_READ_STORAGE);
 
-    public void updateUser(FirebaseUser u) {
-        if (u == null) {
-            createAccount();
-        } else {
-            return;
+            }
         }
     }
 
-    public void createAccount(String email, String password) {
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "createUserWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            updateUser(user);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                            Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                            updateUser(null);
-                        }
-
-                        // ...
-                    }
-                }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResutlts) {
+        if (requestCode == MY_PERMISSIONS_REQUEST_READ_STORAGE) {
+            if (grantResutlts.length > 0
+                    && grantResutlts[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "granted", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "not granted perrmission to read and write to external storage", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 
-    public void singIn(String email, String password) {
-        mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "signInWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            updateUser(user);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "signInWithEmail:failure", task.getException());
-                            Toast.makeText(AuthenticationActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                            updateUser(null);
-                        }
-
-                        // ...
-                    }
-                });
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.sign_in_button:
+                signIn();
+                break;
+        }
     }
-}*/
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            updateUI(account);
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+            updateUI(null);
+        }
+    }
+
+
+    private void updateUI(GoogleSignInAccount account) {
+        if (account == null) {
+            Intent intent0 = new Intent(AuthenticationActivity.this, AuthenticationActivity.class);
+            startActivity(intent0);
+            finish();
+        } else {
+            Intent intent0 = new Intent(AuthenticationActivity.this, ListsActivity.class);
+            startActivity(intent0);
+            finish();
+        }
+    }
 }
---
+++ app/src/main/java/com/example/timad/poznavacka/activities/WelcomeActivity.java
@@ -0,0 +1,49 @@
+package com.example.timad.poznavacka.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.timad.poznavacka.R;
+import com.example.timad.poznavacka.activities.lists.ListsActivity;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class WelcomeActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 1000;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        final GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                updateUI(account);
+            }
+        }, SPLASH_TIME_OUT);
+
+    }
+
+    private void updateUI(GoogleSignInAccount account) {
+        if (account == null) {
+            Intent intent0 = new Intent(WelcomeActivity.this, AuthenticationActivity.class);
+            startActivity(intent0);
+            finish();
+        } else {
+            Intent intent0 = new Intent(WelcomeActivity.this, ListsActivity.class);
+            startActivity(intent0);
+            finish();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+    }
+}
