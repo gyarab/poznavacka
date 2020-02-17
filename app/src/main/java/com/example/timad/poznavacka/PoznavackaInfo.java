ac61e98c0abc602586c84e691aec16f7d74acffd
Jakub <jakub.kucera@student.gyarab.cz>
2020-02-17 21:15:08 +0100

    Some minor stuff.

--- app/src/main/java/com/example/timad/poznavacka/PoznavackaInfo.java
+++ app/src/main/java/com/example/timad/poznavacka/PoznavackaInfo.java
@@ -4,10 +4,12 @@ package com.example.timad.poznavacka;
 public class PoznavackaInfo {
     private String name;
     private String id;
+    private String author;
 
-    public PoznavackaInfo(String name, String id){
+    public PoznavackaInfo(String name, String id, String author){
         this.name = name;
         this.id = id;
+        this.author = author;
     }
 
     public String getName(){
@@ -16,6 +18,10 @@ public class PoznavackaInfo {
 
     public String getId(){
         return id;
+    }
+
+    public String getAuthor(){
+        return author;
     }
 
 }
